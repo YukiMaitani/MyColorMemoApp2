da7e918fba3cafbe433c9de6d6379c90d6485e8e
YukiMaitani <fightzoi1997@gmail.com>
2021-11-25 19:56:43 +0900

    TableViewとCellの設定

--- MyColorMemoApp2/HomeViewController.swift
+++ MyColorMemoApp2/HomeViewController.swift
@@ -9,6 +9,33 @@ import Foundation
 import UIKit
 
 class HomeViewController:UIViewController{
+    @IBOutlet weak var tableView: UITableView!
+    var memoDataList:[MemoDataModel] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        setMemoData()
+    }
     
+    func setMemoData(){
+        for i in 1...5{
+            let memoDataModel = MemoDataModel(text: "\(i)番目のセル", recordDate: Date())
+            memoDataList.append(memoDataModel)
+        }
+    }
 }
 
+extension HomeViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoDataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let memoDataModel:MemoDataModel = memoDataList[indexPath.row]
+        cell.textLabel?.text = memoDataModel.text
+        cell.detailTextLabel?.text = "\(memoDataModel.recordDate)"
+        return cell
+    }
+    
+}
---
+++ MyColorMemoApp2/MemoDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MemoDataModel.swift
+//  MyColorMemoApp2
+//
+//  Created by 米谷裕輝 on 2021/11/25.
+//
+
+import Foundation
+
+struct MemoDataModel{
+    var text:String
+    var recordDate:Date    
+}
